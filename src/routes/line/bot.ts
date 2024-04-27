53f6038af8b4813f0cfe8aa271d1f8194908c52d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-04-27 17:08:49 +0900

    もう少し細かく返すようにした

--- src/routes/line/bot.ts
+++ src/routes/line/bot.ts
@@ -1,5 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { messagingApi, middleware } from '@line/bot-sdk';
+import { messagingApi, middleware, TextMessage } from '@line/bot-sdk';
 const { MessagingApiClient } = messagingApi;
 
 const lineBotRouter = express.Router();
@@ -17,7 +17,7 @@ lineBotRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 lineBotRouter.post('/message', middleware(config), async (req: Request, res: Response, next: NextFunction) => {
   console.log(JSON.stringify(req.body));
-  const result = await Promise.all(req.body.events.map(handleEvent)).catch((err) => {
+  const result = await Promise.all(req.body.events.map(async (event) => handleEvent(event))).catch((err) => {
     console.error(err);
     res.status(200).end();
     Promise.reject(err);
@@ -25,20 +25,30 @@ lineBotRouter.post('/message', middleware(config), async (req: Request, res: Res
   res.json(result);
 });
 
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
+async function handleEvent(event) {
+  if (event.type === 'follow') {
+    const profile = await client.getProfile(event.source.userId);
+    console.log(profile);
     return Promise.resolve(null);
+  } else if (event.type === 'unfollow') {
+    return Promise.resolve(null);
+  } else if (event.type === 'message') {
+    if (event.message.type === 'text') {
+      const echo: TextMessage = { type: 'text', text: event.message.text };
+      return client.replyMessage({
+        replyToken: event.replyToken,
+        messages: [echo],
+      });
+    } else if (event.message.type === 'location') {
+      return Promise.resolve(null);
+    } else if (event.message.type === 'sticker') {
+      const echo: TextMessage = { type: 'text', text: 'sticker message received' };
+      return client.replyMessage({
+        replyToken: event.replyToken,
+        messages: [echo],
+      });
+    }
   }
-
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage({
-    replyToken: event.replyToken,
-    messages: [echo],
-  });
 }
 
 export { lineBotRouter };
