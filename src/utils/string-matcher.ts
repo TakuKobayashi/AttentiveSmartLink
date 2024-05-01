798ff4cd435de79387f15362b69f328678e18d7e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-05-01 13:38:38 +0900

    間違えて消しちゃった

--- src/utils/string-matcher.ts
+++ src/utils/string-matcher.ts
@@ -1,7 +1,12 @@
-import { japanAddressRegExp } from '@/utils/regexp-components';
+import { japanAddressRegExp, urlRegExp } from '@/utils/regexp-components';
 
 const japanAddressRegexp = japanAddressRegExp('g');
+const urlRegexp = urlRegExp('g');
 
 export function matchJapanAddress(text: string): string[] {
   return text.match(japanAddressRegexp) || [];
+}
+
+export function matchHttpUrl(text: string): string[] {
+  return text.match(urlRegexp) || [];
 }
