796ecd251a5cf0cd39ada70ff113b5e1962b9693
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2024-05-02 02:57:36 +0900

    国土地理院のAPIを使うようにした

--- src/utils/geocoding.ts
+++ src/utils/geocoding.ts
@@ -8,16 +8,17 @@ export interface LocationInfo {
 }
 
 export async function convertLocationObjectFromAddress(address: string): Promise<LocationInfo | null> {
-  const response = await axios.get('https://map.yahooapis.jp/geocode/V1/geoCoder', {
-    params: { appid: process.env.YAHOO_API_CLIENT_ID, query: address, output: 'json' },
+  // https://msearch.gsi.go.jp/address-search/AddressSearch は国土地理院のジオサーチAPI
+  const response = await axios.get('https://msearch.gsi.go.jp/address-search/AddressSearch', {
+    params: { q: address },
   });
-  const gecodeData = response.data;
-  const feature = (gecodeData.Feature || [])[0];
+  const gecodeData = response.data || [];
+  const feature = gecodeData[0];
   if (feature) {
-    const [lon, lat] = feature.Geometry.Coordinates.split(',');
+    const [lon, lat] = feature.geometry.coordinates;
     return {
-      title: feature.Name,
-      address: feature.Property.Address,
+      title: feature.properties.title,
+      address: feature.properties.title,
       latitude: lat,
       longitude: lon,
     };
